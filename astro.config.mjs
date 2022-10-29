e813ed560977421777fd4a612c3b8f8b244adf74
Sergey Ufocoder <xufocoder@gmail.com>
2022-10-29 18:34:27 +0300

    Add compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import compress from "astro-compress";
 import { defineConfig } from 'astro/config';
 import { SITE_URL, SITE_BASE } from './src/config';
+
 
 export default defineConfig({
   site: SITE_URL,
   base: SITE_BASE,
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), compress()],
 });
