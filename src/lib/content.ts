d67cddacb64fa4f77aaa561cdecfa07172a55efe
Sergey Ufocoder <xufocoder@gmail.com>
2022-11-01 10:29:31 +0300

    Extract `Typed` component

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -19,3 +19,12 @@ export const addUrlPrefix = (prefix: string = '', path: string = ''): string => 
 }
 
 export const isExternal = (url: string) => url.startsWith('http://') || url.startsWith('https://');
+
+export const shuffleArray = (array) => {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
