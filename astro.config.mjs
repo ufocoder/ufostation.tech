c8f0899e4f7f34d369431a02df1e5f9d002b39f5
Sergey Ufocoder <xufocoder@gmail.com>
2022-11-04 21:28:06 +0300

    Add service worker support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import compress from "astro-compress";
+import serviceWorker from "astrojs-service-worker";
 import { defineConfig } from 'astro/config';
 import { SITE_URL, SITE_BASE } from './src/config';
 import { remarkReadingTime } from './src/lib/remark-reading-time';
 
+const swIntegrationOptions = {
+  workbox: {
+    navigateFallback: 'ru/fallback.html'
+  }
+};
+
 export default defineConfig({
   site: SITE_URL,
   base: SITE_BASE,
-  integrations: [mdx(), sitemap(), compress()],
+  integrations: [mdx(), sitemap(), serviceWorker(swIntegrationOptions), compress()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
     extendDefaultPlugins: true,
