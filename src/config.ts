520c07177ccc5514e268b68817a752176197808d
Christik <christik.lucid@gmail.com>
2023-01-07 15:41:13 +0300

    chore: refactor contributors

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,11 @@ export const SITE_ANALYTICS_ID = '90967695';
 
 export const PODCAST_RSS_URL = 'https://cloud.mave.digital/40984';
 export const PODCAST_SITE_URL = 'https://ufostation.mave.digital';
-export const GITHUB_URL = 'https://github.com/ufocoder/ufostation';
 export const TWITTER_ACCOUNT = '@xufostation';
+
+export const GITHUB_OWNER = 'ufocoder'
+export const GITHUB_REPO = 'ufostation.tech'
+export const GITHUB_URL = `https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}`;
 
 export const DONATE_BOOSTY_URL = 'https://boosty.to/ufocoder';
 export const DONATE_BUYMEACOFFEE_URL = 'https://buymeacoffee.com/xufocoder';
